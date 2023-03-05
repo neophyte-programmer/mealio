b8ca45bd14415bde3b14017c3c5dc5979ad94b55
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-03-05 08:10:36 +0000

    feat: :sparkles: installed react native navigator

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white" >
-      <Text >Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View className="flex-1 items-center justify-center bg-white" >
+
+        <Text >Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
