5e1fc3c379c8692a7bc406c3a94bbb77df2ff4b5
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-03-05 08:17:23 +0000

    feat: :sparkles: setup react navigation

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View className="flex-1 items-center justify-center bg-white" >
-
-        <Text >Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
